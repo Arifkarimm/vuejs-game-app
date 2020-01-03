77fdadd99750f63aab0832b6e98242fd1f5e9f63
Fazlul <fkarif.cse@gmail.com>
2020-01-03 20:05:29 +0100

    step-1: initial attack function work

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ new Vue({
       this.isGameLoading = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+    attack() {
+      let min = 3;
+      let max = 10;
+      let damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth -= damage;
+
+      min = 3;
+      max = 10;
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.monsterHealth -= damage;
     }
   }
 });
