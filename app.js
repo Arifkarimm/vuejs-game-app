7d721bd3eb8b8a8baac26e483b36c8c758033613
Fazlul <fkarif.cse@gmail.com>
2020-01-03 20:49:18 +0100

    step-3: checkresult function set up

--- app.js
+++ app.js
@@ -14,26 +14,19 @@ new Vue({
     attack() {
       let damage = this.calculationDamage(3, 10);
       this.playerHealth -= damage;
-      if (this.playerHealth <= 0) {
-        alert("You loss");
+      if (this.checkResult()) {
+        return;
       }
-
       this.monsterHealthDamage();
-      if (this.monsterHealth <= 0) {
-        alert("You Win");
-      }
     },
     specialAttack() {
       let damage = this.calculationDamage(10, 20);
       this.playerHealth -= damage;
-      if (this.playerHealth <= 0) {
-        alert("You loss");
+      if (this.checkResult()) {
+        return;
       }
 
       this.monsterHealthDamage();
-      if (this.monsterHealth <= 0) {
-        alert("You Win");
-      }
     },
     monsterHealthDamage() {
       let damage = this.calculationDamage(5, 12);
@@ -41,6 +34,24 @@ new Vue({
     },
     calculationDamage(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkResult() {
+      if (this.playerHealth <= 0) {
+        if (confirm("You Win ! Start New Game")) {
+          this.startGame();
+        } else {
+          this.isGameLoading = false;
+        }
+        return true;
+      } else if (this.monsterHealth <= 0) {
+        if (confirm("You Loss ! Start New Game")) {
+          this.startGame();
+        } else {
+          this.isGameLoading = false;
+        }
+        return true;
+      }
+      return false;
     }
   }
 });
