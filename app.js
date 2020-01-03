7fe1159865125d4dd1f036a4fb2cc229ee012663
Fazlul <fkarif.cse@gmail.com>
2020-01-03 21:00:44 +0100

    heal funktionality set up

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ new Vue({
     },
     attack() {
       let damage = this.calculationDamage(3, 10);
-      this.playerHealth -= damage;
+      this.monsterHealth -= damage;
       if (this.checkResult()) {
         return;
       }
@@ -21,30 +21,37 @@ new Vue({
     },
     specialAttack() {
       let damage = this.calculationDamage(10, 20);
-      this.playerHealth -= damage;
+      this.monsterHealth -= damage;
       if (this.checkResult()) {
         return;
       }
-
+      this.monsterHealthDamage();
+    },
+    heal() {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
       this.monsterHealthDamage();
     },
     monsterHealthDamage() {
       let damage = this.calculationDamage(5, 12);
-      this.monsterHealth -= damage;
+      this.playerHealth -= damage;
     },
     calculationDamage(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
     checkResult() {
       if (this.playerHealth <= 0) {
-        if (confirm("You Win ! Start New Game")) {
+        if (confirm("You Loss ! Start New Game")) {
           this.startGame();
         } else {
           this.isGameLoading = false;
         }
         return true;
       } else if (this.monsterHealth <= 0) {
-        if (confirm("You Loss ! Start New Game")) {
+        if (confirm("You Win ! Start New Game")) {
           this.startGame();
         } else {
           this.isGameLoading = false;
