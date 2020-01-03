a51789698e96c324652534c4e8c4a25ea85196a9
Fazlul <fkarif.cse@gmail.com>
2020-01-03 19:59:05 +0100

    start button set as toogle

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ new Vue({
     isGameLoading: false,
     playerHealth: 100,
     monsterHealth: 100
+  },
+  methods: {
+    startGame() {
+      this.isGameLoading = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    }
   }
 });
