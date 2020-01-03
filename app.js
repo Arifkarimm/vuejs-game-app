d4f5d78766be359e491914d7d2a22f4a0bdd891a
Fazlul <fkarif.cse@gmail.com>
2020-01-03 21:21:27 +0100

    final touch

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ new Vue({
   data: {
     isGameLoading: false,
     playerHealth: 100,
-    monsterHealth: 100
+    monsterHealth: 100,
+    turns: []
   },
   methods: {
     startGame() {
       this.isGameLoading = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack() {
       let damage = this.calculationDamage(3, 10);
       this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Monster Damage " + damage
+      });
       if (this.checkResult()) {
         return;
       }
@@ -22,6 +28,10 @@ new Vue({
     specialAttack() {
       let damage = this.calculationDamage(10, 20);
       this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Monster special Damage " + damage
+      });
       if (this.checkResult()) {
         return;
       }
@@ -33,6 +43,10 @@ new Vue({
       } else {
         this.playerHealth = 100;
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player Heal +10"
+      });
       this.monsterHealthDamage();
     },
     giveUp() {
@@ -41,6 +55,11 @@ new Vue({
     monsterHealthDamage() {
       let damage = this.calculationDamage(5, 12);
       this.playerHealth -= damage;
+      this.checkResult();
+      this.turns.unshift({
+        isPlayer: false,
+        text: "Player Damage " + damage
+      });
     },
     calculationDamage(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
